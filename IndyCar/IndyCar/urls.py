70c8bdbefda3739a50c4300b54a78a7130c100be
Kenny Almeida <kenny_almeida_25@hotmail.com>
2019-08-21 08:26:08 -0300

    adc tela de login

--- IndyCar/IndyCar/urls.py
+++ IndyCar/IndyCar/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from funilaria.views import *
+from funilaria import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user),
     path('',cliente),
     path('formcliente/', novocliente, name='formcliente'),
 ]
--- IndyCar/funilaria/views.py
+++ IndyCar/funilaria/views.py
@@ -3,9 +3,12 @@ from django.http import request,HttpResponseRedirect
 from funilaria.forms import ClienteForm
 from django.contrib import messages
 from funilaria.models import Customer
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-
+@login_required(login_url='/login/' )
 def cliente(request):
     clientes = Customer.objects.all().order_by('id')
     return render(request,'index.html',context={'clientes':clientes})
@@ -23,3 +26,25 @@ def novocliente(request):
         form = ClienteForm()
         return render(request,'formulario_indy_car.html',context={'form':form})
 
+def login_user(request):
+    return render(request, 'login.html')
+
+
+@csrf_protect
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username)
+        print(password)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'Usuário ou senha inválido')
+    return redirect('/login/')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/login/')
