f5997581ca7fb7195d95958aa82ee93d707a6126
Erick Nathan Panse <1800560@SALAS.AULAS>
2019-08-23 08:50:28 -0300

    arrumando o forms 3

--- funilaria/forms.py
+++ funilaria/forms.py
@@ -1,41 +1,4 @@
 from django import forms
-from funilaria.models import Cliente
-
-class ClienteForm(forms.ModelForm):
-    cpf = forms.CharField(max_length=9,label='CPF:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o cpf',
-        'name':'cpf',
-        'id':'cpf'}))
-    nome = forms.CharField(max_length=60,label='Nome completo:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o nome',
-        'name':'nome',
-        'id':'nome'}))
-    endereco = forms.CharField(max_length=100,label='Endereço:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o endereço',
-        'name':'endereco',
-        'id':'endereco'}))
-    bairro = forms.CharField(max_length=8,label='bairro:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o bairro',
-        'name':'bairro',
-        'id':'bairro'}))
-    email = forms.EmailField(max_length=60,label='Email:',widget = forms.EmailInput(attrs={
-        'placeholder':'informe o seu email',
-        'name':'email',
-        'id':'email'}))
-    rg = forms.CharField(max_length=9,label='rg:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o seu rg',
-        'name':'rg',
-        'id':'rg'}))
-    tel = forms.CharField(max_length=15,label='tel:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o seu tel',
-        'name':'tel',
-        'id':'tel'}))
-
-    class Meta:
-        model = Cliente
-        fields={'cpf','nome','endereco','bairro','email','rg','tel'}
-
-from django import forms
 from funilaria.models import Cliente,Empresa,Veiculo
 
 class ClienteForm(forms.ModelForm):
@@ -52,15 +15,15 @@ class ClienteForm(forms.ModelForm):
         'name':'bairro',
         'id':'bairro'}))
     email = forms.EmailField(max_length=60,label='Email:',widget = forms.EmailInput(attrs={
-        'placeholder':'informe o seu email',
+        'placeholder':'informe o email',
         'name':'email',
         'id':'email'}))
     rg = forms.CharField(max_length=9,label='rg:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o seu rg',
+        'placeholder':'informe o rg',
         'name':'rg',
         'id':'rg'}))
     tel = forms.CharField(max_length=15,label='tel:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o seu tel',
+        'placeholder':'informe o tel',
         'name':'tel',
         'id':'tel'}))
 
@@ -82,15 +45,15 @@ class EmpresaForm(forms.ModelForm):
         'name':'bairro',
         'id':'bairro'}))
     email = forms.EmailField(max_length=60,label='Email:',widget = forms.EmailInput(attrs={
-        'placeholder':'informe o seu email',
+        'placeholder':'informe o email',
         'name':'email',
         'id':'email'}))
     cnpj = forms.CharField(max_length=9,label='rg:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o seu rg',
-        'name':'rg',
-        'id':'rg'}))
+        'placeholder':'informe o cnpj',
+        'name':'cnpj',
+        'id':'cnpj'}))
     tel = forms.CharField(max_length=15,label='tel:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o seu tel',
+        'placeholder':'informe o tel',
         'name':'tel',
         'id':'tel'}))
 
@@ -100,33 +63,33 @@ class EmpresaForm(forms.ModelForm):
 
 class VeiculoForm(forms.ModelForm):
     marca = forms.CharField(max_length=10,label='Nome completo:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o nome',
-        'name':'nome',
-        'id':'nome'}))
+        'placeholder':'informe a marca',
+        'name':'marca',
+        'id':'marca'}))
     modelo = forms.CharField(max_length=20,label='Endereço:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o endereço',
-        'name':'endereco',
-        'id':'endereco'}))
+        'placeholder':'informe o modelo',
+        'name':'modelo',
+        'id':'modelo'}))
     cor = forms.CharField(max_length=10,label='bairro:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o bairro',
-        'name':'bairro',
-        'id':'bairro'}))
+        'placeholder':'informe a cor',
+        'name':'cor',
+        'id':'cor'}))
     placa = forms.EmailField(max_length=7,label='Email:',widget = forms.EmailInput(attrs={
-        'placeholder':'informe o seu email',
-        'name':'email',
-        'id':'email'}))
+        'placeholder':'informe a placa',
+        'name':'placa',
+        'id':'placa'}))
     ano = forms.CharField(max_length=4,label='rg:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o seu rg',
-        'name':'rg',
-        'id':'rg'}))
+        'placeholder':'informe o ano',
+        'name':'ano',
+        'id':'ano'}))
     cidade = forms.CharField(max_length=10,label='tel:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o seu tel',
-        'name':'tel',
-        'id':'tel'}))
+        'placeholder':'informe a cidade',
+        'name':'cidade',
+        'id':'cidade'}))
     estado = forms.CharField(max_length=2,label='tel:',widget = forms.TextInput(attrs={
-        'placeholder':'informe o seu tel',
-        'name':'tel',
-        'id':'tel'}))
+        'placeholder':'informe o estado',
+        'name':'estado',
+        'id':'estado'}))
 
     entrada = forms.DateTimeField(label='tel:',widget = forms.widgets.DateTimeInput(format="%d %b %Y %H:%M:%S %Z"))
     
