4480484f81ab0818080595552fefab9102da54a8
Erick Nathan Panse <1800560@SALAS.AULAS>
2019-09-20 10:44:04 -0300

    mateiral

--- funilaria/models.py
+++ funilaria/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse 
-# Create your models here.
+# Create your models here.ou não
 
 class Customer(models.Model):
     nome = models.CharField(max_length=60)
