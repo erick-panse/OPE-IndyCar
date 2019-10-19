f136b7c37242a2b988fb3eda18e5635b972d7ad3
ph0980 <pedrohenrique009800@gmail.com>
2019-10-19 13:41:38 -0300

    exemplo de msg on front

--- IndyCar/settings.py
+++ IndyCar/settings.py
@@ -144,3 +144,12 @@ EMAIL_HOST_USER = 'tstmail92@gmail.com'
 EMAIL_HOST_PASSWORD = 'mailtst2019'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
